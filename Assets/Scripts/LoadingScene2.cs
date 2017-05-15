3929cf328acbcbe69a9f0d390a4ed4e1a5ce3475
MrD00M <camilo.villegas@upb.edu.co>
2017-05-15 11:07:08 -0500

    Ver en Trello y Wapp Cambios!

--- Assets/Scripts/LoadingScene2.cs
+++ Assets/Scripts/LoadingScene2.cs
@@ -5,40 +5,32 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LoadingScene2 : MonoBehaviour {
-	public Texture2D ProgressBar;
-	private float tiempo;
-	private AsyncOperation async = null;
-	private GUIStyle escenaDos;
-	public string a, b;
+	private float timer;
+	public Text Uno, Dos;
+	private Color Color1, Color2;
+	private string a;
+	public Object scene;
 	//Always start this coroutine in the start function
 	private void Start()
 	{
-		tiempo = 5;
-		StartCoroutine(LoadLevel(a));
-		escenaDos = new GUIStyle ();
-		escenaDos.fontSize = 200;
-		escenaDos.normal.textColor = Color.white;
+		Color1 = Uno.color;
+		Color2 = Dos.color;
 	}
 	//CoRoutine to return async progress, and trigger level load.
-	private IEnumerator LoadLevel(string Level)
-	{
-		yield return new WaitForSeconds (5);
-		Application.LoadLevelAsync(Level);
-		yield return async;
-	}
-	private void OnGUI()
-	{
-			GUI.Label (new Rect (220, 0, 100, 100), b, escenaDos);
-			GUI.Label (new Rect (220, 200, 50, 50), "Cargando...", escenaDos);
-		// Check if it's loading;
-		if (async != null)
-		{
-			GUI.DrawTexture(new Rect(0, 0, 100 * async.progress, 50), ProgressBar);
-		}
-	}
 
 	private void Update(){
-		tiempo -= Time.deltaTime;
+		timer += Time.deltaTime;
+		if (timer >= 1) {
+			Color1.a += 0.5f * Time.deltaTime;
+			Uno.color = Color1;
+		}
+		if (timer >= 3) {
+			Color2.a += 0.5f * Time.deltaTime;
+			Dos.color = Color2;
+		}
+		if (timer >= 10) {
+			SceneManager.LoadScene (scene.name);
+		}
 	
 	}
 }
---
+++ Assets/Scripts/RPB.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RPB : MonoBehaviour {
+
+    public Transform barraproreso;
+    public Transform indicadortexto;
+    public Transform cargatexto;
+    [SerializeField]
+    private float currentAmount;
+    [SerializeField]
+    private float speed;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (currentAmount < 100)
+        {
+            currentAmount += speed * Time.deltaTime;
+            indicadortexto.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
+            cargatexto.gameObject.SetActive(true);
+        }
+        else
+        {
+            cargatexto.gameObject.SetActive(false);
+            indicadortexto.GetComponent<Text>().text = "Listo!";
+
+        }
+        barraproreso.GetComponent<Image>().fillAmount = currentAmount / 100;
+	}
+}
